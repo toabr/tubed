6d8d116e8f637cde80b94f14e722fc766d220beb
c3to <c3to@web.de>
2018-11-08 12:56:30 +0100

    Updates

---
+++ precache-manifest.a3a7e277e73bf700b1d34ded3a59b6c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/yt-telly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "c52e2362783be7de802c",
+    "url": "/yt-telly/static/css/main.a8610e0e.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/yt-telly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fbcd42d534aae391c16e",
+    "url": "/yt-telly/static/js/1.fbcd42d5.chunk.js"
+  },
+  {
+    "revision": "a15be8f8a53ffc5246d2",
+    "url": "/yt-telly/static/js/runtime~main.a15be8f8.js"
+  },
+  {
+    "revision": "c52e2362783be7de802c",
+    "url": "/yt-telly/static/js/main.c52e2362.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/yt-telly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/yt-telly/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/yt-telly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fbcd42d534aae391c16e",
+    "url": "/yt-telly/static/css/1.001c429a.chunk.css"
+  },
+  {
+    "revision": "0496c91b593432c6c3752d1e2aa991c5",
+    "url": "/yt-telly/index.html"
+  }
+];
